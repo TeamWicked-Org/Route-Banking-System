e2a5579d66a27ccc1af0ff4ead0c377ae43682d2
rahma-nagy <rahmanagy11sep@gmail.com>
2026-06-07 14:16:59 +0200

    create person class

--- Banking System/include/models/Person.h
+++ Banking System/include/models/Person.h
@@ -1,6 +1,31 @@
 #pragma once
 #include "utils.h"
-class Person {
+#include <string>
 
+class Person
+{
+protected:
+    int id;
+    std::string name;
+    std::string password;
+
+public:
+  // Constructors
+    Person();
+    Person(int id, std::string name, std::string password);
+
+  // Setters
+    void setName(std::string name);
+    void setPassword(std::string password);
+
+  // Getters
+    int getId() const;
+    std::string getName() const;
+    std::string getPassword() const;
+
+  // Display
+    virtual void display() const;
+
+  // Destructor
+    virtual ~Person();
 };
-
--- Banking System/src/models/Person.cpp
+++ Banking System/src/models/Person.cpp
@@ -1,2 +1,54 @@
 #include "../../include/models/Person.h"
 
+Person::Person()
+{
+    id = 0;
+    name = "";
+    password = "";
+}
+
+Person::Person(int id, std::string name, std::string password)
+{
+    this->id = id;
+    setName(name);
+    setPassword(password);
+}
+
+void Person::setName(std::string name)
+{
+    if (utils::Validation::validateName(name))
+        this->name = name;
+}
+
+void Person::setPassword(std::string password)
+{
+    if (utils::Validation::validatePassword(password))
+    {
+        this->password = utils::Security::stringHash(password);
+    }
+}
+
+int Person::getId() const
+{
+    return id;
+}
+
+std::string Person::getName() const
+{
+    return name;
+}
+
+std::string Person::getPassword() const
+{
+    return password;
+}
+
+void Person::display() const
+{
+    std::cout << "ID: " << id << std::endl;
+    std::cout << "Name: " << name << std::endl;
+}
+
+Person::~Person()
+{
+}
