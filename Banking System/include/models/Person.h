21fa08270496fe9a167d973ed5fc41e26313049f
rahma-nagy <rahmanagy11sep@gmail.com>
2026-06-09 15:43:44 +0200

    create person class

--- Banking System/include/models/Person.h
+++ Banking System/include/models/Person.h
@@ -1,31 +1,33 @@
-#pragma once
-#include "utils.h"
-#include <string>
-
-class Person
-{
-protected:
-    int id;
-    std::string name;
-    std::string password;
-
-public:
-  // Constructors
-    Person();
-    Person(int id, std::string name, std::string password);
-
-  // Setters
-    void setName(std::string name);
-    void setPassword(std::string password);
-
-  // Getters
-    int getId() const;
-    std::string getName() const;
-    std::string getPassword() const;
-
-  // Display
-    virtual void display() const;
-
-  // Destructor
-    virtual ~Person();
-};
+#pragma once 
+#include "utils.h" 
+#include <iostream> 
+#include <string> 
+using namespace std; 
+ 
+class Person 
+{ 
+private: 
+    int id; 
+    std::string name; 
+    std::string password; 
+ 
+public: 
+  // Constructors 
+    Person(); 
+    Person(int id, std::string name, std::string password); 
+ 
+  // Setters 
+    void setName(std::string name); 
+    void setID(int id); 
+    void setPassword(std::string password); 
+ 
+  // Getters 
+    virtual std::string getName() const; 
+    virtual std::string getPassword() const; 
+    virtual int getID() const; 
+ 
+  // Display : Pure Virtual 
+    virtual void display() const = 0; 
+ 
+}; 
+ 
--- Banking System/src/models/Person.cpp
+++ Banking System/src/models/Person.cpp
@@ -1,54 +1,37 @@
-#include "../../include/models/Person.h"
-
-Person::Person()
-{
-    id = 0;
-    name = "";
-    password = "";
-}
-
-Person::Person(int id, std::string name, std::string password)
-{
-    this->id = id;
-    setName(name);
-    setPassword(password);
-}
-
-void Person::setName(std::string name)
-{
-    if (utils::Validation::validateName(name))
-        this->name = name;
-}
-
-void Person::setPassword(std::string password)
-{
-    if (utils::Validation::validatePassword(password))
-    {
-        this->password = utils::Security::stringHash(password);
-    }
-}
-
-int Person::getId() const
-{
-    return id;
-}
-
-std::string Person::getName() const
-{
-    return name;
-}
-
-std::string Person::getPassword() const
-{
-    return password;
-}
-
-void Person::display() const
-{
-    std::cout << "ID: " << id << std::endl;
-    std::cout << "Name: " << name << std::endl;
-}
-
-Person::~Person()
-{
+#include "../../include/models/Person.h" 
+ 
+// Constructors initializer list 
+Person::Person() : id(0), name(""), password(""){} 
+ 
+Person::Person(int id, std::string name, std::string password) : id(id), name(name), password(utils::Security::stringHash(password)) {} 
+ 
+ 
+void Person::setName(std::string name) 
+{ 
+    this->name = name; 
+} 
+ 
+void Person::setID(int id) 
+{ 
+    this->id = id; 
+} 
+ 
+void Person::setPassword(std::string password) 
+{ 
+    this->password = utils::Security::stringHash(password); 
+} 
+ 
+std::string Person::getName() const 
+{ 
+    return name; 
+} 
+ 
+std::string Person::getPassword() const 
+{ 
+    return password; 
+} 
+ 
+int Person::getID() const 
+{ 
+    return id; 
 }
