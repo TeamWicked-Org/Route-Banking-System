5b2d0d38afab5c51fc54a9b162587651b43ffc19
Renadamer <nonaamer294@gmail.com>
2026-06-10 23:11:49 +0300

    Add employee class

--- Banking System/include/models/Employee.h
+++ Banking System/include/models/Employee.h
@@ -2,6 +2,13 @@
 #include "Person.h"
 class Employee : public Person {
 
+private:
+	double salary;
+
+public:
+	void setsalary(double salary);
+	double getsalary();
+	void display();
 
 };
 
--- Banking System/src/models/Employee.cpp
+++ Banking System/src/models/Employee.cpp
@@ -1,1 +1,18 @@
 #include "../../include/models/Employee.h"
+
+void Employee::setsalary(double s) {
+	if (s < 5000) {
+		cout << "minimum salary is 5000 !" << endl;
+	}
+	salary = s;
+}
+
+double Employee::getsalary() {
+	return salary;
+}
+
+void Employee::display() {
+	cout << "ID: " << getID() << endl;
+	cout << "Name: " << getName() << endl;
+	cout << "Salary: " << salary << endl;
+}
