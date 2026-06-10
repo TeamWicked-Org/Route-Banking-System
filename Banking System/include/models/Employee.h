3e8a12aa8f03d236436897e5e2d8b236d105300e
Mohammed-Arhab <178622671+Mohammed-Arhab@users.noreply.github.com>
2026-06-11 00:11:10 +0300

    Removed stashed notifiers

--- Banking System/include/models/Employee.h
+++ Banking System/include/models/Employee.h
@@ -1,9 +1,7 @@
-
 #pragma once
 #include "Person.h"
 class Employee : public Person {
 
-<<<<<<< Updated upstream
 private:
 	double salary;
 
@@ -12,6 +10,4 @@ public:
 	double getsalary();
 	void display();
 
-=======
->>>>>>> Stashed changes
 };
--- Banking System/src/models/Employee.cpp
+++ Banking System/src/models/Employee.cpp
@@ -1,6 +1,5 @@
 #include "../../include/models/Employee.h"
 
-<<<<<<< Updated upstream
 void Employee::setsalary(double s) {
 	if (s < 5000) {
 		cout << "minimum salary is 5000 !" << endl;
@@ -16,7 +15,4 @@ void Employee::display() {
 	cout << "ID: " << getID() << endl;
 	cout << "Name: " << getName() << endl;
 	cout << "Salary: " << salary << endl;
-=======
-\
->>>>>>> Stashed changes
 }
