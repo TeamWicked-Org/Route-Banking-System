04491fc472a98c219fa2aab7daeeb6ab9aacdd88
Mohammed-Arhab <178622671+Mohammed-Arhab@users.noreply.github.com>
2026-06-11 08:48:16 +0300

    Removed Employee Comments

--- Banking System/include/models/Employee.h
+++ Banking System/include/models/Employee.h
@@ -27,13 +27,3 @@ public:
 	void display() const override;
 
 };
-
-
-// Renad Forgot the following:
-// 1- Constructors
-// 2- Employee info getters
-// 3- Employee info setters (Username + passwords)
-// 4- display don't have override keyword
-// 5- Static Employee's id counter to be viewable and shared across all Employee instances
-// 6- Salary checking didn't use utils::Validation::validateSalary from utils class
-// 7- Salary checking will be done by the caller not inside the function
